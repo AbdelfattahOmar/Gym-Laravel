3acae052600451243846e2cb1624d24bdb5f5209
oeladawy <omnia1699@gmail.com>
2022-05-04 21:30:08 +0200

    seeders gym manager

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,8 @@ Route::delete('/city/{cityID}', [CityController::class, 'destroy'])->name('city.
 Route::get('/restoredCities', [CityController::class, 'showDeleted'])->name('city.showDeleted')->middleware('auth')->middleware('role:admin');
 Route::get('/restoredCities/{postID}', [CityController::class, 'restore'])->name('city.restored')->middleware('auth')->middleware('role:admin');
 
+// gym manager routes
+
 
 Route::get('/TrainingSessions/index', [TrainingSessionController::class, 'index'])->name('trainingSession.listSessions')->middleware('auth');
 
