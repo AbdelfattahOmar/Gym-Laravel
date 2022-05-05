741078198382b11148138e647fa8d0a153b11d51
oeladawy <omnia1699@gmail.com>
2022-05-05 03:15:25 +0200

    update 2

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <!-- Main Sidebar Container -->
 @role('admin|cityManager|gymManager')
- <aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
      <!-- Brand Logo -->
      <a href="index3.html" class="brand-link">
          <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
