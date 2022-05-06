47399b051d1bd613a05b1c265bf26982fccd205c
reem-elbakry <reemmousa1995@gmail.com>
2022-05-06 22:09:35 +0200

    fix error

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -233,7 +233,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ route('PaymentPackage.stripe'') }}" class="nav-link">
+                            <a href="{{ route('PaymentPackage.stripe') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Buy a package.</p>
                             </a>
